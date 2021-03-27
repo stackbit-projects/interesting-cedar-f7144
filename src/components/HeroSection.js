a0e5029c4149132f975b1995d15c69a6bf22104b
Stackbit Code Editor <projects@stackbit.com>
2021-03-27 17:58:50 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,6 +11,7 @@ export default class HeroSection extends React.Component {
         return (
             <section className="hero bg-color" data-id={_.get(section, 'section_id', null)}>
                 {_.get(section, 'background_image', null) && (
+                <div className="hero__bg-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'background_image', null)) + '\'); opacity: ' + bg_img_opacity + ';')}/>
                 )}
                 <div className="hero__links link-group">
                   {_.map(_.get(section, 'actions', null), (action, action_idx) => {
